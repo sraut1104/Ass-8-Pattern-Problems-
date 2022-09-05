e2fdcb46da2867603cb48206d8199240a6e0c634
sraut1104 <94961695+sraut1104@users.noreply.github.com>
2022-09-05 21:38:36 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,43 @@
+/*   Star Pattern-4
+        1234321
+         12321
+          121
+           1
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j,x=0,y;
+    for(i=1;i<=4;i++)
+    {
+        y=i-x;
+        for(j=1;j<=7;j++)
+        {
+            if(j>=i && j<=8-i)
+            {
+                if(j>=1 && j<=4)
+                {
+                    printf("%d",y);
+                    y++;
+                }
+                if(j==5)
+                {
+                    y=y-2;
+                    printf("%d",y);
+                }
+                if(j>=6 && j<=7)
+                {
+                    y--;
+                    printf("%d",y);
+                }
+            }
+            else
+                printf(" ");
+        }
+        x++;
+        printf("\n");
+    }
+
+    return 0;
+}

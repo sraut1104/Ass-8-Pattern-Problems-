9242dd8905b265416d5ebc0c776eafd441a2a614
SUDIP RAUT <94961695+sraut1104@users.noreply.github.com>
2022-09-05 23:21:40 +0530

    Add files via upload

---
+++ 15.c
@@ -0,0 +1,37 @@
+/*   Star Pattern-15
+            *
+           **
+          * *
+         *  *
+        *****
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j>=6-i && j<=5)
+            {
+                if (i==3 || i==4)
+                {
+                    if(j>=7-i && j<=4)
+                        printf(" ");
+                    else
+                        printf("*");
+                }
+
+                else
+                    printf("*");
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
